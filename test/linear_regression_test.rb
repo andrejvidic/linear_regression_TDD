3ae72edac0412d4290ae9f90f6e47b1df9f7214a
drej2k <andrevidic@hotmail.com>
2015-06-23 17:15:40 +1000

    add minitest boilerplate code

---
+++ test/linear_regression_test.rb
@@ -0,0 +1,10 @@
+require '../linear_regression.rb'
+require 'minitest/autorun'
+
+#  optional pretty output that reads like a specificication
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new()
+
+class TestRegressionLine < Minitest::Test
+
+end
