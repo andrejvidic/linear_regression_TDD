2838a4c7a487cb0d6becccc20c3ea71aaac96785
drej2k <andrevidic@hotmail.com>
2015-06-23 17:38:02 +1000

    add sum(x^2) code to make test pass

---
+++ linear_regression.rb
@@ -0,0 +1,12 @@
+class RegressionLine
+
+  def initialize(x_array, y_array)
+    @x = x_array
+    @y = y_array
+    @n = x_array.length
+  end
+
+  def sum_x_squared
+    @x.map {|e| e*e }.reduce(:+)
+  end
+end
